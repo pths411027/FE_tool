c5efb352624cea218a746a134818d6ccb722e209
Marcus <pths411027@gmail.com>
2023-08-21 09:48:20 +0800

    Add workflow

--- src/pages/DataSuite/index.js
+++ src/pages/DataSuite/index.js
@@ -1,26 +1,21 @@
 import React, { createContext, useState, useContext } from 'react';
 
-import Title from "./components/Title"
+import Title from "./components/Title";
+import Layer2 from "./components/Layer2";
 import Editor from "./components/Editor";
+import WorkFlow from "./components/WorkFlow";
 import "./index.css"
-import { Form, Select, Button, Dropdown, Modal, Label} from 'semantic-ui-react';
-
-
 
 const Datasuite = () => {
     const [mode, setMode] = useState('none'); // 預設為登入模式
-   
+
 
     return (
         <div>
-        <Title />
-        
-        <Editor />
-       
-        
-        
-        
-        
+            <Title />
+            <Layer2 mode={mode} setMode={setMode} />
+            {mode === 'Editor' && <Editor />}
+            {mode === 'WorkFlow' && <WorkFlow />}
         </div>
     );
 }
